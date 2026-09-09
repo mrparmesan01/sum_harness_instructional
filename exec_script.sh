646cfc467930ad6cca6b87241280600764ae4d3c
type <type>
2026-09-08 23:45:09 -0700

    Fixed execution script

--- exec_script.sh
+++ exec_script.sh
@@ -5,10 +5,5 @@ export OMP_PROC_BIND=spread
 
 #run the application:
 srun -n 1 -c 256 --cpu_bind=cores build/sum_direct sum_direct 1
-srun -n 1 -c 256 --cpu_bind=cores build/sum_vector sum_vector 1
 srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect sum_indirect 1
-srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect sum_indirect 2
-srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect sum_indirect 3
-srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect_seed sum_indirect_seed 1
-srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect_seed sum_indirect_seed 2
-srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect_seed sum_indirect_seed 3
+srun -n 1 -c 256 --cpu_bind=cores build/sum_vector sum_vector 1
