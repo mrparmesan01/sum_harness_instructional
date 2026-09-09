3ced905b6961e614d05652831573f9ea37dfdd1b
type <type>
2026-09-08 23:45:35 -0700

    Fixed some of the sum_vector logic

--- exec_script.sh
+++ exec_script.sh
@@ -1,8 +1,3 @@
-#OpenMP settings:
-export OMP_NUM_THREADS=1
-export OMP_PLACES=threads
-export OMP_PROC_BIND=spread
-
 #run the application:
 srun -n 1 -c 256 --cpu_bind=cores build/sum_direct sum_direct 1
 srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect sum_indirect 1
